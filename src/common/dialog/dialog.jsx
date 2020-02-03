60784b92ce66fe0c145fbe7001bf213a746c7d3c
igor.fedoseev <igor.fedoseev@intexsoft.by>
2020-02-03 14:43:36 +0300

    dialog

--- src/common/dialog/dialog.jsx
+++ src/common/dialog/dialog.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import "./dialog.css"
+import "./dialog.scss"
 
 export const Dialog = () => {
     return (
-        <div>
-            Dialog
+        <div className='dialog'>
+            <div className='dialog-item'>
+                <div className='item'>Dmitri</div>
+                <div className='item'>Anrey</div>
+                <div className='item'>Igor</div>
+                <div className='item'>Dmitri</div>
+                <div className='item'>Anrey</div>
+                <div className='item'>Igor</div>
+            </div>
+            <div className='dialog-mesages'>
+                <div className='mesage'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, velit?</div>
+                <div className='mesage'>Lorem ipsum dolor sit amet.</div>
+                <div className='mesage'>Lorem, ipsum dolor.</div>
+                <div className='mesage'>Lorem ipsum dolor sit, amet consectetur adipisicing.</div>
+                <div className='mesage'>Lorem ipsum dolor sit amet consectetur.</div>
+                <div className='mesage'>Lorem ipsum dolor sit.</div>
+            </div>
         </div>
     )
 };
